b31893e64aacf116b62912f28fe3ca6ff29032d1
NormalKnight <minercrew1233@gmail.com>
2026-02-05 18:44:24 +0800

    Shift Assignment Update
    > Updated Spaghetti Code
    - Programme now checks for a duplicate on the selected Sheet.

--- Mall_Employee_Scheduling_System/src/Main.java
+++ Mall_Employee_Scheduling_System/src/Main.java
@@ -66,126 +66,138 @@ public class Main {
 	}
 	
 	public static void AssignShift() {
-		int targetSheet = 0, targetRow = 0;
-		int employeeRow = 0;
-		String shiftEntry = null;
+		//XXX: Waiting for Input Validation
 		
-		
+				int targetSheet = 0, targetRow = 0;
+				int employeeRow = 0;
+				String shiftEntry = null;
+				
 		System.out.println(">> Assigning Employee to Shift:");
-		
-		
-		employeeSelect: while(true) { // Tags outer loop for easy breaking.
-		if(input != null && input.equals("cancel")) break; //Automatically exits if user types cancel once.
-		
-		System.out.println("> Select Employee to Assign:");
-		
-		seeEmployeeList();
-		System.out.println("[!] Type 'cancel' to return to menu.");
-		
-		input = scan.nextLine();
-		
-		//Loop through the employees 2D array for a match.
-		for (int i = 0; i < employees.length; i++) {
+				
+				
+				employeeSelect: while(true) { // Tags outer loop for easy breaking.
+				if(input != null && input.equals("cancel")) break; //Automatically exits if user types cancel once.
+				
+				System.out.println("> Select Employee to Assign:");
+				
+				seeEmployeeList();
+				System.out.println("[!] Type 'cancel' to return to menu.");
+				
+				input = rd.nextLine();
+				
+				//Loop through the employees 2D array for a match.
+				for (int i = 0; i < employees.length; i++) {
 
-			//If match is found:
-			if (input.equals(employees[i][0])) {
-				System.out.println("> ID match for " + input + " found.");
-				shiftEntry = employees[i][1] + "(" + employees[i][2] + ")";
-				employeeRow = i;
-				break employeeSelect; //Break out of the Outer while loop
+					//If match is found:
+					if (input.equals(employees[i][0])) {
+						System.out.println("> ID match for " + input + " found.");
+						shiftEntry = employees[i][1] + "(" + employees[i][2] + ")";
+						employeeRow = i;
+						break employeeSelect; //Break out of the Outer while loop
+						
+					} else if((i + 1) == employees.length)System.out.println("> No ID match for " + input + " found. Did you make a typo?");
+					 // ^ Only displays on the last loop.
+				}
 				
-			} else if((i + 1) == employees.length)System.out.println("> No ID match for " + input + " found. Did you make a typo?");
-			 // ^ Only displays on the last loop.
-		}
-		
-		}
-		
-		daySelect: while(true) { // Tags outer loop for easy breaking.
-		if(input != null && input.equals("cancel")) break;
-		
-		System.out.println("> Select Day to add Employee's shift to (MONDAY-" + daySlotHeader[days-1] + "):");
-		System.out.println("[!] Type 'cancel' to return to menu.");
-		
-		input = scan.nextLine().toLowerCase();
-		
-		switch(input) {
-		case "monday":
-			targetSheet = 0;
-			System.out.println("> Shift will be added to " + input + ".");
-			break daySelect; //Breaks outer loop.
-		case "tuesday":
-			targetSheet = 1;
-			break daySelect;
-		case "wednesday":
-			targetSheet = 2;
-			break daySelect;
-		case "thursday":
-			targetSheet = 3;
-		case "friday":
-			targetSheet = 4;
-			break daySelect;
-		case "saturday":
-			targetSheet = 5;
-			break daySelect;
-		case "cancel":			
-			break daySelect;
-		default:
-			System.out.println("> " + input + " is not a valid day. Did you make a typo?");
-			break;
-		}
-		}
-		
-		
-		
-		timeSelect: while(true) { // Tags outer loop for easy breaking.
-			if(input != null && input.equals("cancel")) break timeSelect;
-			
-			System.out.println("> Select Time slot to add the shift to:");
-			System.out.println("(M: Morning, A: Afternoon, E: Evening)");
-			
-			input = scan.nextLine().toLowerCase();
-			
-			switch(input) {
-			case "m":
-				targetRow = 0;
-				break timeSelect; // Breaks outer loop.
-			case "a":
-				targetRow = 1;
-				break timeSelect;
-			case "e":
-				targetRow = 2;
-				break timeSelect;
-			default:
-				System.out.println("> " + input + " is not a valid time slot. Did you make a typo?");
-				break;
-			
-			}
-			
-		}
-		
-		if(input != null && input.equals("cancel")) { input = null; return; }
-		// Check if timeSlot of selected shift has available spaces.)
-		
-		for(int i = 0; i < shiftSchedule[targetSheet][targetRow].length; i++) {
-			// If a matching String is found in the row:
-			if(shiftSchedule[targetSheet][targetRow][i].equals(shiftEntry)) {
-				System.out.println("> " + employees[employeeRow][1] + " is already assigned to this time slot!");
-				break;
-			} 
-			// If the slot is empty.
-			if(shiftSchedule[targetSheet][targetRow][i] != null &&
-			shiftSchedule[targetSheet][targetRow][i].equals("-")) {
+				}
 				
-				shiftSchedule[targetSheet][targetRow][i] = shiftEntry;
-				System.out.println("> " + employees[employeeRow][1] + " was assigned to " + timeSlotHeader[targetRow] + " slot for"+  daySlotHeader[targetSheet] + ".");
-				break;
-			} else if((i+1) == shiftSchedule[targetSheet][targetRow].length)
-				System.out.println("> Couldn't assign " + employees[employeeRow][1] + " to shift. " + daySlotHeader[targetSheet]+"'s " + timeSlotHeader[targetRow]+ " slot is full.");
 				
-			
-		}
-		
+				
+				daySelect: while(true) { // Tags outer loop for easy breaking.
+				if(input != null && input.equals("cancel")) break;
+				
+				System.out.println("> Select Day to add Employee's shift to (MONDAY-" + daySlotHeader[days-1] + "):");
+				System.out.println("[!] Type 'cancel' to return to menu.");
+				
+				input = rd.nextLine().toLowerCase();
+				
+				switch(input) {
+				case "monday":
+					targetSheet = 0;
+					System.out.println("> Shift will be added to " + input + ".");
+					break daySelect; //Breaks outer loop.
+				case "tuesday":
+					targetSheet = 1;
+					break daySelect;
+				case "wednesday":
+					targetSheet = 2;
+					break daySelect;
+				case "thursday":
+					targetSheet = 3;
+				case "friday":
+					targetSheet = 4;
+					break daySelect;
+				case "saturday":
+					targetSheet = 5;
+					break daySelect;
+				case "cancel":			
+					break daySelect;
+				default:
+					System.out.println("> " + input + " is not a valid day. Did you make a typo?");
+					break;
+				}
+				}
+				
+				
+				
+				timeSelect: while(true) { // Tags outer loop for easy breaking.
+					if(input != null && input.equals("cancel")) break timeSelect;
+					
+					System.out.println("> Select Time slot to add the shift to:");
+					System.out.println("(M: Morning, A: Afternoon, E: Evening)");
+					
+					input = rd.nextLine().toLowerCase();
+					
+					switch(input) {
+					case "m":
+						targetRow = 0;
+						break timeSelect; // Breaks outer loop.
+					case "a":
+						targetRow = 1;
+						break timeSelect;
+					case "e":
+						targetRow = 2;
+						break timeSelect;
+					default:
+						System.out.println("> " + input + " is not a valid time slot. Did you make a typo?");
+						break;
+					
+					}
+					
+				}
+				
+				if(dupeChecker(shiftEntry, targetSheet)) { // Calls the method that checks if a duplicate entry exists.
+					System.out.println("> Employee is already assigned to a shift for this day!");
+					return; // Go back to main(), skip the assignment code.
+				} 
+				
+				if(input != null && input.equals("cancel")) {input = null; return;}
+				for(int i = 0; i < shiftSchedule[targetSheet][targetRow].length; i++) {
+					
+					
+					if(shiftSchedule[targetSheet][targetRow][i].equals("-")) // Checks if the slot is empty.
+					{
+						shiftSchedule[targetSheet][targetRow][i] = shiftEntry;
+						System.out.println("> " + employees[employeeRow][1] + " was assigned to " + timeSlotHeader[targetRow] + " slot for "+  daySlotHeader[targetSheet] + ".");
+						break;
+					} else if((i+1) == shiftSchedule[targetSheet][targetRow].length) { // Display on the last iteration.
+						System.out.println("> Couldn't assign " + employees[employeeRow][1] + " to shift. " + daySlotHeader[targetSheet]+"'s " + timeSlotHeader[targetRow]+ " slot is full."); 
+					}
+				
+				}
+				
 	}
+	
+	// XXX: Might be modifiable to double as the search feature.
+		public static boolean dupeChecker(String target, int sh) {
+				for(int row = 0; row < shiftSchedule[sh].length; row++) {
+					for(int col = 0; col < shiftSchedule[sh][row].length; col++) {
+						
+						if(shiftSchedule[sh][row][col].equals(target)) return true;
+							
+						}}
+				return false;
+			}
 	
 	
 	public static void SeeList(int sheetIndex) { // sheetIndex is the 3D array's face(Programmer defined.)
